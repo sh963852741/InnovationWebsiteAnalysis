f3f1dec401fdfc552733d3f1cf032fdf96f5357a
宋润涵 <sh963852741@126.com>
2020-09-05 19:09:33 +0800

    分离数据

---
+++ js/testdata.js
@@ -0,0 +1,60 @@
+let data = {
+    mainChart: {
+        concluded: [9, 5, 4, 1, 6, 11, 7, 4, 8, 3],
+        established: [3, 9, 1, 4, 7, 6, 8, 7, 10, 4],
+        studentCount: [30, 25, 93, 74, 69, 105, 53, 50, 91, 60]
+    },
+    detailChart1: {
+        data: [{
+            name: '软件工程系',
+            children: [{
+                name: '2016级',
+                value: 3
+            }, {
+                name: '2017级',
+                value: 8
+            }]
+        }, {
+            name: '人工智能系',
+            children: [{
+                name: '2016级',
+                value: 6
+            }, {
+                name: '2017级',
+                value: 9
+            }]
+        }, {
+            name: '信息与通信工程系',
+            children: [{
+                name: '2016级',
+                value: 4
+            }, {
+                name: '2017级',
+                value: 2
+            }]
+        }, {
+            name: '网络空间安全系',
+            children: [{
+                name: '2016级',
+                value: 2
+            }, {
+                name: '2017级',
+                value: 8
+            }]
+        }, {
+            name: '计算机科学系',
+            children: [{
+                name: '2016级',
+                value: 8
+            }, {
+                name: '2017级',
+                value: 5
+            }]
+        }]
+    },
+    detailChart2: {},
+    detailChart3: {
+        concluded: [3, 4, 7, 8, 3, 2, 1, 6],
+        established: [3, 5, 9, 7, 8, 1, 6, 4],
+    },
+}
